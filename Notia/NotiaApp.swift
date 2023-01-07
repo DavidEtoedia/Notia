e6323c0623433a65d7d1ff966b31eb143ec96eac
DavidEtoedia <etoedia@gmail.com>
2023-01-07 13:07:18 -0800

    Initial Commit

---
+++ Notia/NotiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiaApp.swift
+//  Notia
+//
+//  Created by Inyene Etoedia on 07/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
